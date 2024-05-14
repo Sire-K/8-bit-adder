75f38e8bd615846b4d2bef86e2ee11ab2d186d4c
Sire-K <150567674+Sire-K@users.noreply.github.com>
2024-05-14 08:10:26 +0545

    Update half_adder.py

--- half_adder.py
+++ half_adder.py
@@ -3,16 +3,16 @@ from inputs import assignment
 
 def logic(A, B, C):
 
-    if C == True:                                    #Sets C as False
+    if C == True:                                        # Sets C as False
         C = False
-    add = (A ^ B)                                    #XOR GATE: A XOR B
-    carry = (A * B)                                      #AND GATE: A AND B            
+    add = (A ^ B)                                        # XOR GATE: A XOR B
+    carry = (A * B)                                      # AND GATE: A AND B            
 
     add_var, carry_var = bool_operation(add, carry)
     print(f"Add: {add_var}, Carry: {carry_var}")
 
 def bool_operation(add, carry):
-    add_variable = bool(add)                                  #Sets add as True if 1 and False if 0
+    add_variable = bool(add)                             # Sets add as True if 1 and False if 0
     carry_variable = bool(carry)
-    return add_variable, carry_variable                                    #Sets C as True if 1 and False if 0
+    return add_variable, carry_variable                  # Sets C as True if 1 and False if 0
 
